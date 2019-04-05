8c99b8a2db0c7cec7cfc9433fd745852d9e684a9
Kosolapov Vadim <vadkos33@outlook.com>
2019-04-05 17:34:43 +0200

    Update user form

--- src/Controller/admin/UserController.php
+++ src/Controller/admin/UserController.php
@@ -3,8 +3,10 @@
 namespace App\Controller\admin;
 
 use App\Entity\User;
+use App\Form\admin\UserEditForm;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @Route("/admin")
@@ -26,12 +28,37 @@ class UserController extends AbstractController
     }
 
     /**
-     * @Route("/users/{id}", name="user_by_id", requirements={"id"="\d"})
+     * @Route("/users/{id}", name="view_user", requirements={"id"="\d"})
      */
-    public function showUser(User $user)
+    public function viewUser(User $user)
     {
         return $this->render('admin/user/one-user.html.twig',[
             'user' => $user
+        ]);
+    }
+
+    /**
+     * @Route("/users/update/{id}", name="delete_user")
+     */
+    public function updateUser(User $user, Request $request)
+    {
+        $form = $this->createForm(UserEditForm::class, $user);
+        $form->handleRequest($request);
+
+        $user = new User();
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setFullname($form->get('fullname')->getData());
+            $user->setUsername($form->get('username')->getData());
+            $user->setEmail($form->get('email')->getData());
+            $user->setStatus($form->get('status')->getData());
+
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+        }
+
+        return $this->render('admin/user/update.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
