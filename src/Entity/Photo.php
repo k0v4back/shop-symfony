69da31c651e874df272f8b6865fa4071c53f359a
Kosolapov Vadim <vadkos33@outlook.com>
2019-04-06 14:51:32 +0200

    Product photo

---
+++ src/Entity/Photo.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PhotoRepository")
+ */
+class Photo
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $product_id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getProductId(): ?int
+    {
+        return $this->product_id;
+    }
+
+    public function setProductId(int $product_id): self
+    {
+        $this->product_id = $product_id;
+
+        return $this;
+    }
+}
---
+++ src/Services/photo/ProductPhoto.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Services\photo;
+
+use App\Entity\Photo;
+use Doctrine\ORM\EntityManagerInterface;
+
+class ProductPhoto
+{
+    /** @var EntityManagerInterface */
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function createPhoto(string $name, int $product_id)
+    {
+        $photo = new Photo();
+        $photo->setName($name);
+        $photo->setProductId($product_id);
+
+        $em = $this->entityManager;
+        $em->persist($photo);
+        $em->flush();
+
+        return $photo;
+    }
+
+    public function updatePhoto(string $name, int $product_id)
+    {
+        $photo = new Photo();
+        $photo->setName($name);
+        $photo->setProductId($product_id);
+
+        $em = $this->entityManager;
+        $em->flush();
+    }
+
+    public function deletePhoto(Photo $photo)
+    {
+        $em = $this->entityManager;
+        $em->remove($photo);
+    }
+}
