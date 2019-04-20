257164372ad9dc73e504df6e33478841c0a1130d
Kosolapov Vadim <vadkos33@outlook.com>
2019-04-20 19:41:50 +0200

    Sort photo

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use App\Repository\ChoiceRepository;
+use App\Repository\PhotoRepository;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,13 +17,18 @@ class ProductController extends AbstractController
     /** @var ChoiceRepository */
     private $choiceRepository;
 
+    /** @var PhotoRepository */
+    private $photoRepository;
+
     public function __construct(
         ProductRepository $productRepository,
-        ChoiceRepository $choiceRepository
+        ChoiceRepository $choiceRepository,
+        PhotoRepository $photoRepository
     )
     {
         $this->productRepository = $productRepository;
         $this->choiceRepository = $choiceRepository;
+        $this->photoRepository = $photoRepository;
     }
 
     /**
@@ -45,11 +51,13 @@ class ProductController extends AbstractController
     public function oneProduct(Product $product)
     {
         $choices = $this->choiceRepository->findBy(['product' => $product], array('sort' => 'ASC'));
+        $photos = $this->photoRepository->findBy(['product' => $product], array('sort' => 'ASC'));
         return $this->render(
             'one-product.html.twig',
             [
                 'product' => $product,
                 'choices' => $choices,
+                'photos' => $photos,
             ]
         );
     }
