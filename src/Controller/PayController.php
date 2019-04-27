384532d9b917cbea6f18f16f88f55e5ae35279ca
Kosolapov Vadim <vadkos33@outlook.com>
2019-04-27 14:37:12 +0200

    Pay page

--- src/Controller/PayController.php
+++ src/Controller/PayController.php
@@ -3,22 +3,33 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Repository\BasketRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PayController extends AbstractController
 {
-    /**
-     * @Route("/pay/{id}/{quantity}/{price}", name="pay_page")
-     */
-    public function main(Product $product, $quantity, $price)
+    /** @var BasketRepository */
+    public $basketRepository;
+
+    public function __construct
+    (
+        BasketRepository $basketRepository
+    )
     {
+        $this->basketRepository = $basketRepository;
+    }
+
+    /**
+     * @Route("/pay/{id}", name="pay_page")
+     */
+    public function main(Product $product)
+    {
+        $all = $this->basketRepository->findOneBy(['product' => $product]);
         return $this->render(
             'pay/index.html.twig',
             [
-                'product' => $product,
-                'quantity' => $quantity,
-                'price' => $price
+                'product' => $all,
             ]
         );
     }
